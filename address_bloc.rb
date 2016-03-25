961f384917b97df6f48dfe65025af3171b000e68
Jigesh Parekh <JParekh117@gmail.com>
2016-03-25 14:22:50 -0700

    Checkpoint 20 Create menucontroller handle input and  entries submenu

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+#create new menucontroller when addressbloc starts
+system "clear"
+#system clear the command line
 puts "Welcome to AddressBloc"
+#call main menu to display the menu
+menu.main_menu
